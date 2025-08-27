bbedbb8f3d7c59485287312a8cc3dffecf696dc5
Yash Kumar <k.yash20005@gmail.com>
2025-08-27 17:06:09 +0000

    shpow error

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import AuthForm from '@/components/auth-form';
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
